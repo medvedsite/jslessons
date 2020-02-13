eed82bc31542f33e661f4631924abc409244e764
asdf <geniusneo2310@gmail.com>
2020-02-13 19:01:01 +0200

    testiki

--- test.js
+++ test.js
@@ -1,13 +1,24 @@
 'use strict';
 
-let name,
-    admin;
-name = 'john';
-admin = name;
+let money = +prompt("Ваш бюджет на месяц?","");
+let time = +prompt("Введите дату в формате YYYY-MM-DD");
 
-alert(admin);
-let answer = confirm('are you huy?');
-console.log(answer);
-let otvet = prompt('tebe est 18?');
-console.log(otvet);
-console.log(typeof (otvet));
+
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
+    savings : false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+    
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget/30);
