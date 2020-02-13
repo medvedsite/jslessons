b9fce04e8f2083f05096380c474ffd8ce4f8204b
asdf <geniusneo2310@gmail.com>
2020-02-13 16:51:20 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let name,
+    admin;
+name = 'john';
+admin = name;
+
+alert(admin);
+let answer = confirm('are you huy?');
+console.log(answer);
+let otvet = prompt('tebe est 18?');
+console.log(otvet);
+console.log(typeof(otvet));
